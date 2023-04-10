d1dba4e11b6660a6b4fbbe935f347788e0bc2681
Jollyroger <roger.suuressaar@gmail.com>
2023-04-10 12:38:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RogerSuur.github.io
---
+++ server/server.js
@@ -0,0 +1,63 @@
+
+import express, { json } from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000; 
+
+app.use(cors());
+
+app.use(json())
+
+// Enable CORS
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// Define a route for the proxy
+app.get('/proxy', async (req, res) => {
+  // Make the request to the remote API
+
+  console.log('app.get /proxy route initiated')
+  try {
+  //gets values from the client
+  const authHeader = req.headers.authorization;
+  const encodedCredentials = authHeader.split(' ')[1];
+  const decodedCredentials = Buffer.from(encodedCredentials, 'base64').toString();
+  const [username, password] = decodedCredentials.split(':');
+  // Make the request to the remote API
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Basic " + encodedCredentials
+  };
+
+
+  const response = await fetch('https://01.kood.tech/api/auth/signin', {
+    method: 'POST',
+    headers
+   // Pass the query parameters as the request body
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  // Get the response data and send it back to the client
+  const data = await response.json();
+  const jwt_token = data;
+  res.json({ token: jwt_token });
+
+} catch (error) {
+  console.error(`Error occurred: ${error}`);
+  res.status(401).json({ message: 'Invalid credentials. Please try again.' });
+}
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
